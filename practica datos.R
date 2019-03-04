2b9789e3be9832b947efecd71f012acf91e65dbf
neyrm <neromu27@gmail.com>
2019-03-04 14:04:51 -0600

    Agregar archivos
    Archivos csv con errores y sin errores

---
+++ practica datos.R
@@ -0,0 +1,12 @@
+base<-read.csv("Practica_ODD19_SCiAC_errores.csv", sep = ";", dec = ".", header = T)
+
+head(base)
+
+attach(base)
+
+summary(base)
+
+mean(base$Ta_corporal, na.rm = T) #na.rm= es para que no tome en cuenta los NA
+sd(base$Ta_corporal, na.rm = T)
+
+boxplot(Ta_corporal~Zona, xlab="Zona", ylab= "Temperatura corporal (C)", outline= FALSE)

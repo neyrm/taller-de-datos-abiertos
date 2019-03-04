49d27983b3c9fba446786adb44ab165516389f34
neyrm <neromu27@gmail.com>
2019-03-04 14:22:14 -0600

    Agregar una línea de código en script
    Cambiar un dato para ver efecto en el programa

--- practica datos.R
+++ practica datos.R
@@ -10,3 +10,5 @@ mean(base$Ta_corporal, na.rm = T) #na.rm= es para que no tome en cuenta los NA
 sd(base$Ta_corporal, na.rm = T)
 
 boxplot(Ta_corporal~Zona, xlab="Zona", ylab= "Temperatura corporal (C)", outline= FALSE)
+
+mean(base$Alt_Suelo)
